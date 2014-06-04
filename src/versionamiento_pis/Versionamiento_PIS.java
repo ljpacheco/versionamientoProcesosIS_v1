a554d80e3dd4d03f08273a5c2ed72f4d377d5d52
katty-henrry <katty-henrry@katty.utpl.edu.ec>
2014-06-03 21:23:14 -0500

    Version 1.2. Metodos: Crear y Escribir archivo .txt con datos de una persona

--- src/versionamiento_pis/Versionamiento_PIS.java
+++ src/versionamiento_pis/Versionamiento_PIS.java
@@ -6,17 +6,48 @@
 
 package versionamiento_pis;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author katty-henrry
  */
 public class Versionamiento_PIS {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
+    static String nombre="Lily";
+    static String apellidos="Pacheco Jara";
+    static String ci="1105210627";
+    static String estadoCivil="Soltera";
+    static String telefono="072664281";
+    static String direccion="IV Centenario";
+    static String ocupacion="Estudiante";
+    static String carrera="Ingenieria en sistemas y Computación";
+    static String ciclo="Decimo";
     
-}
+    // Metodos -  Crear y escribir archivos con datos personales
+    static String ruta="C:\\Users\\katty-henrry\\Desktop\\DatosPersonales.txt";
+     public static void main(String[] args) throws IOException {
+         File archivo = new File(ruta);
+         if (!archivo.exists()) {
+             BufferedWriter escribir = new BufferedWriter(new FileWriter(ruta));
+             escribir.write("************ DATOS PERSONALES ************" + "\n" );
+             escribir.write("Nombres: " + nombre + "\n");
+             escribir.write("Apellidos: " + apellidos + "\n");
+             escribir.write("Telefono: " + telefono + "\n");
+             escribir.write("Cedula: "  + ci);
+             escribir.write("Carrera/Titulacion: " + carrera + "\n");
+             escribir.write("Ciclo: " + ciclo + "\n" );
+             escribir.close();
+         }else{
+             System.out.println("El archivo ya se encuentra creado, cambie el nombre o la ruta");
+          
+         }
+      }      
+    }
